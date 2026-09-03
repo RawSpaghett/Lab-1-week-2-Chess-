eb30a32735ba56773aaf1ecf9e398053f169bf1e
RawSpaghett <rawspaghetti05@gmail.com>
2026-09-03 17:29:25 -0400

    BoardManager

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    [SerializeField] private int gridSize = 8;
+    private Color light = Color.white;
+    private Color dark = Color.black;
+
+    void OnDrawGizmos()
+    {
+        for(int r = 0; r < gridSize; r++)
+        {
+            for(int c = 0; c < gridSize; c++)
+            {
+                Gizmos.color = ((r+c)%2 == 0) ? light: dark;
+                Gizmos.DrawCube(new Vector3(r ,c,0),new Vector3(1,1,1));
+            }
+        }
+    }
+}
